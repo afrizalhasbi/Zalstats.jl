797cfb83934e97562d8ae3c6a5910b5010c3ae93
afrizalhasbi <afrizal.azizy@gmail.com>
2025-07-27 05:58:44 +0700

    wip kendall

--- src/common/rank.jl
+++ src/common/rank.jl
@@ -20,13 +20,45 @@ function rankorder(m::AbstractMatrix{<:Real})
     return result
 end
 
-function get_concdisc_pairs(a::AbstractVector, b::AbstractVector)
-    @assert length(a) == length(b) "Vector lengths must be equal!"
-    N = length(a)
-    a2, a, b2, b = a[1:N-1], a[2:end], b[1:N-1], b[2:end]
-    A, B = sign.(a .- a2), sign.(b .- b2)
-    disc = sum(A .!= B)
-    conc = N - 1 - disc
+function get_concdisc_pairs(v::AbstractMatrix{T}, target::AbstractMatrix{T}) where {T<:Real}
+    @assert size(v)[2] == 2 "Source must be an m * 2 matrix!"
+    @assert size(target) == (1, 2) "Target must be a single row of 2 columns!"
 
-    return conc, disc
+    tiesvec = (v .== target)   # bitvector buffer with values 0, 1 where 1 = tied
+
+    # count rows where both columns are tied
+    # int8 casting is used to minimize memory use
+    # cast to int8 before summing, otherwise
+    # it will create a temporary int64 vector
+    tiesvec = sum(eachcol(Int8.(tiesvec)))
+    ties = sum(tiesvec .== 2)
+
+
+    x_larger = v[:, 1] .> target[1, 1]  # source x > target_x
+    y_larger = v[:, 2] .> target[1, 2]  # source y > target_y
+
+    # both true or both false (same direction)
+    concvec = x_larger .== y_larger
+
+    # as 1 = tied, a row is non-concordant if the
+    # value changes when tiesvec is added elwise
+    # we now get a new bitvector where 1 = concordant
+    concvec = (concvec .+ tiesvec) .== concvec
+    conc = sum(Int8.(concvec))
+
+    # simply get disc by subtracting num rows
+    # with ties and conc
+    disc = size(v)[1] - conc - ties
+    return [conc, disc, ties]
+end
+
+function get_concdisc_pairs(a::AbstractVector, b::AbstractVector)
+    cdt = [0, 0, 0]
+    matrix = hcat(a, b)
+    for idx in eachindex(a) - 1
+        tmp_cdt = get_concdisc_pairs(matrix[idx:end, :], matrix[idx, :])
+        cdt .+= tmp_cdt
+    end
+    conc, disc, ties = cdt[1], cdt[2], cdt[3]
+    conc, disc, ties
 end
--- src/correlation/kendall.jl
+++ src/correlation/kendall.jl
@@ -1,32 +1,32 @@
 include("../common/common.jl")
 
-function kendall(x::AbstractVector{<:Real}, y::AbstractVector{<:Real}, hypothesis::Union{String,Nothing}=nothing)
+function kendalla(x::AbstractVector{<:Real}, y::AbstractVector{<:Real}, hypothesis::Union{String,Nothing}=nothing)
     @assert length(x) == length(y) "Vector lengths must be equal"
     @assert (hypothesis in ["frequentist", "bayesian", nothing]) "Hypothesis test must be 'frequentist', 'bayesian', or Nothing!"
 
-    N = length(x)
-    conc, disc = get_concdisc_pairs(x, y)
-    numer = 2 * disc
-    denom = (N * (N - 1)) / 2
-    corr = 1 - (numer / denom)
+    conc, disc, ties = get_concdisc_pairs(x, y)
+    numer = conc - disc
+    denom = length(x)
+
+    corr = numer / denom
     return corr
 end
 
 
-function kendall(formula::Formula, hypothesis::Union{String,Nothing}=nothing)
+function kendalla(formula::Formula, hypothesis::Union{String,Nothing}=nothing)
     x = formula.x
     y = formula.y
-    kendall(x, y, hypothesis)
+    kendalla(x, y, hypothesis)
 end
 
-function kendall(matrix::Matrix{<:Real})
+function kendalla(matrix::Matrix{<:Real})
     len = length(eachcol(matrix))
     corrs = []
     for (idx, col) in enumerate(eachcol(matrix))
         if idx == len
             break
         else
-            push!(corrs, kendall(col |> Vector, matrix[:, idx+1] |> Vector))
+            push!(corrs, kendalla(col |> Vector, matrix[:, idx+1] |> Vector))
         end
     end
     return to_symm(corrs)
